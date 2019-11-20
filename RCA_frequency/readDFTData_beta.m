4418cb62896017c194809c30f7d3343d9229bf57
Alexandra Yakovleva <leksea@gmail.com>
2019-11-20 15:11:20 -0800

    info AY

--- RCA_frequency/readDFTData_beta.m
+++ RCA_frequency/readDFTData_beta.m
@@ -1,6 +1,8 @@
 function varargout = readDFTData_beta(varargin)
 % Alexandra Yakovleva, Stanford University 2012-2020.
 
+% loads up project's frequency data in a single sell matrix nSubjecs x nConditions
+
     eegSrcDir = varargin{1}.srcEEG;
     eegMatDir = varargin{1}.loadedEEG;
     dataType = 'RLS';

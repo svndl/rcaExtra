84ecb5deb56c3aaa8fc5f37a74363ed9788c2906
Alexandra Yakovleva <leksea@gmail.com>
2019-11-20 15:03:00 -0800

    info AY

--- RCA_frequency/getRealImag_byBin.m
+++ RCA_frequency/getRealImag_byBin.m
@@ -1,5 +1,7 @@
 function [data_re, data_im] = getRealImag_byBin(inputData, nBins, nFreqs, nChs)
-% Alexandra Yakovleva, Stanford University 2012-2020.
+% Alexandra Yakovleva, Stanford University 2012-2020
+% part of averageFrequencyData toolbox
+
     [data_cos, data_sin] = getRealImag(inputData);
     data_re = cellfun(@(x) reshape(x, [nBins nFreqs nChs size(x, 3)]), data_cos, 'uni', false);
     data_im = cellfun(@(x) reshape(x, [nBins nFreqs nChs size(x, 3)]), data_sin, 'uni', false);

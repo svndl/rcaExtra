0db4132baaf134f7b2002a223626328ff6951baa
Alexandra Yakovleva <leksea@gmail.com>
2019-11-20 14:32:19 -0800

    info AY

--- RCA_time/baselineDataTime.m
+++ RCA_time/baselineDataTime.m
@@ -1,5 +1,8 @@
 function out = baselineDataTime(data, idIdx)
 
+% Alexandra Yakovleva, Stanford University 2012-2020
+% baselining time data
+
     m1 = nanmean(data(1:idIdx));   
     out = data - m1;
 end

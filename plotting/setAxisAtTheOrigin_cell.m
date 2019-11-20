16ffc878ba67a83aa9bf1c576fbf3de36f891366
Alexandra Yakovleva <leksea@gmail.com>
2019-11-20 14:27:12 -0800

    info AY

--- plotting/setAxisAtTheOrigin_cell.m
+++ plotting/setAxisAtTheOrigin_cell.m
@@ -1,4 +1,8 @@
 function setAxisAtTheOrigin_cell(axisCell)
+% Alexandra Yakovleva, Stanford University 2012-2020
+
+% Center axis for a cell array of handles 
+
     for nax = 1:numel(axisCell)
         setAxisAtTheOrigin(axisCell{nax});
     end

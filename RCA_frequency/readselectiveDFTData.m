3d938bcc3db600b8515f88be297f4b3b3eb90dee
Alexandra Yakovleva <leksea@gmail.com>
2020-03-31 13:10:19 -0700

    Update readselectiveDFTData.m
    Subj naming/update

--- RCA_frequency/readselectiveDFTData.m
+++ RCA_frequency/readselectiveDFTData.m
@@ -6,13 +6,14 @@ function [subjs, sensorData, cellNoiseData1, cellNoiseData2, info] = readselecti
      loadedData = pathStruct.loadedEEG;     
      
      %% list subjects
-     list_subj = list_folder(fullfile(sourceData, [settings.subjTag '*']));
+     list_subj = list_folder(fullfile(sourceData, settings.subjTag));
      nsubj = numel(list_subj);
      
      %% pre-allocate
      sensorData = {};
      cellNoiseData1 = {};
      cellNoiseData2 = {};
+     info = [];
         
     subjs = {list_subj(:).name};
     subjLoaded = ones(nsubj, 1);
@@ -39,9 +40,9 @@ function [subjs, sensorData, cellNoiseData1, cellNoiseData2, info] = readselecti
                     try
                         [signalData, noise1, noise2, info] = extractDataSubset(fullfile(loadedData, processedDataFileName), settings); 
                         
-                        sensorData(s, :) = signalData;
-                        cellNoiseData1(s, :) = noise1;
-                        cellNoiseData2(s, :) = noise2;
+                        sensorData(s, :) = signalData';
+                        cellNoiseData1(s, :) = noise1';
+                        cellNoiseData2(s, :) = noise2';
                         
                         if (isfield(settings, 'saveAsNewMatFile'))
                             sourceDataFileName_new = sprintf('%s_%s_%s.mat', list_subj(s).name, dataType, settings.saveAsNewMatFile);
@@ -49,7 +50,8 @@ function [subjs, sensorData, cellNoiseData1, cellNoiseData2, info] = readselecti
                         end
                        
                     catch err
-                        display(['Warning, could not process data from: ' list_subj(s).name]);                      
+                        display(['Warning, could not process data from: ' list_subj(s).name]);
+                        subjLoaded(s) = 0;                       
                     end
                 catch err
                     display(['Warning, could not load data from: ' list_subj(s).name]);
@@ -61,4 +63,7 @@ function [subjs, sensorData, cellNoiseData1, cellNoiseData2, info] = readselecti
         end
     end
     subjs = subjs(subjLoaded>0);
+    sensorData = sensorData(subjLoaded>0, :);
+    cellNoiseData1 = cellNoiseData1(subjLoaded>0, :);
+    cellNoiseData2 = cellNoiseData2(subjLoaded>0, :);  
 end

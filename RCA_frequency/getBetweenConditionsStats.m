44cec473be116c7d05744bcd071b1736c7129ca1
Alexandra Yakovleva <leksea@gmail.com>
2020-04-15 14:40:51 -0700

    Update getBetweenConditionsStats.m
    fixed error when haandling multiple RC components

--- RCA_frequency/getBetweenConditionsStats.m
+++ RCA_frequency/getBetweenConditionsStats.m
@@ -1,6 +1,17 @@
-function [sig, pVal, stat] = getBetweenConditionsStats(inputData, nBs, nFs, W)
+function stats = getBetweenConditionsStats(inputData, nBs, nFs, W)
 % Alexandra Yakovleva, Stanford University 2012-2020.
 
-    [avgSubj_Re, avgSubj_Im] = averageSubjectFrequencyData(inputData, nBs, nFs, W);   
-    [sig, pVal, stat] = computeStatsFreq(cat(3, avgSubj_Re{:}), cat(3, avgSubj_Im{:})); 
+    [avgSubj_Re, avgSubj_Im] = averageSubjectFrequencyData(inputData, nBs, nFs, W); 
+    nRcs = size(W, 2);
+    sig = zeros(nFs, nRcs);
+    pVal = zeros(nFs, nRcs);
+    stat = zeros(nFs, nRcs);
+    for rc = 1:nRcs
+        avgSubj_re_cat = cat(3, avgSubj_Re{:});
+        avgSubj_im_cat = cat(3, avgSubj_Im{:});
+        [sig(:, rc), pVal(:, rc), stat(:, rc)] = computeStatsFreq(avgSubj_re_cat(:, rc, :), avgSubj_im_cat(:, rc, :)); 
+    end
+    stats.sig = sig;
+    stats.pValues = pVal;
+    stats.statistics = stat;
 end

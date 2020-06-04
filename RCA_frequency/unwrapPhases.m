6eb9775b3a169d03675fd3723f6a58ca31c5aa26
leksea <leksea@gmail.com>
2020-06-04 00:01:03 -0700

    Removed fixed dim for phase unwrapping

--- RCA_frequency/unwrapPhases.m
+++ RCA_frequency/unwrapPhases.m
@@ -1,23 +1,16 @@
 function out = unwrapPhases(values)
     out = values;
-    for c = 1:size(values, 2)
-        %2f
-        while (out(2, c)<out(1, c))
-            out(2, c) = out(2, c) + 2*pi;
-        end
-        % 3f
-        while (out(3, c)<out(2, c))
-            out(3, c) = out(3, c) + 2*pi;
-        end
-        %4f    
-        while (out(4, c) < out(3, c))
-            out(4, c) = out(4, c) + 2*pi;
+    [nF, nCnd] = size(values);
+    for c = 1:nCnd
+        for f = 2:nF
+            while (out(f, c) < out(f-1, c))
+                out(f, c) = out(f, c) + 2*pi;
+            end
         end
 
-        if(out(4, c) - out(3, c) < pi/2)
-            out(4, c) = out(4, c) + 2*pi;
+        if(out(end, c) - out(end - 1, c) < pi/2)
+            out(end, c) = out(end, c) + 2*pi;
         end
     end
-    
 end
 

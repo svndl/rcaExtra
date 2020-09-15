b1203094397668bdcfa19e51893d4c103afddf1c
leksea <leksea@gmail.com>
2020-09-15 09:46:01 -0700

    Latency computation update
    Added subj, proj level. Bug fixes.

--- frequency/rcaExtra_computeLatencies.m
+++ frequency/rcaExtra_computeLatencies.m
@@ -7,7 +7,6 @@ function [latencyVals, latencyErrs] = rcaExtra_computeLatencies(rcaResult)
 
 % rcaResult.rcaSettings.useFrequencies  -- cell array of harmonic multiples
 % rcaResult.rcaSettings.useFrequenciesHz -- double value of stimulus frequency
-% rcaResult.rcaSettings.nComp -- number of components
 % rcaResult.projAvg.phase -- angle values, 3D matrix of nF x nRc x nCnd
 
 
@@ -17,12 +16,14 @@ function [latencyVals, latencyErrs] = rcaExtra_computeLatencies(rcaResult)
 % latencyErrs 2D array, computed latency error estimates (msec), symmetric,  nRCs x nCnd
 
     freqIdx = cellfun(@(x) str2double(x(1)), rcaResult.rcaSettings.useFrequencies, 'uni', true);
-    freqVals = rcaResult.rcaSettings.useFrequenciesHz*freqIdx;
-    nCnd = size(rcaResult.projAvg.phase, 3);
-    latencyVals = zeros(rcaResult.rcaSettings.nComp, nCnd);
-    latencyErrs = zeros(rcaResult.rcaSettings.nComp, nCnd);
-    for cp = 1:numel(rcaResult.rcaSettings.nComp)
-        rcaAngles = squeeze(rcaResult.projAvg.phase(:, rc, :));
+    freqVals = rcaResult.rcaSettings.useFrequenciesHz*freqIdx';
+    nF = numel(freqIdx);
+    [~, nComp, nCnd] = size(rcaResult.projAvg.phase);
+    latencyVals = zeros(nComp, nCnd);
+    latencyErrs = zeros(nComp, nCnd);
+    for rc = 1:nComp
+        
+        rcaAngles = squeeze(rcaResult.projAvg.phase(freqIdx, rc, :));
         values_unwrapped = unwrapPhases(rcaAngles);
         for c = 1:nCnd
 
@@ -30,12 +31,12 @@ function [latencyVals, latencyErrs] = rcaExtra_computeLatencies(rcaResult)
        
             yfit = Pc(1)*freqVals + Pc(2);
     
-            latencyVals(cp, c) = 1000*Pc(1)/(2*pi);    
+            latencyVals(rc, c) = 1000*Pc(1)/(2*pi);    
             d = (yfit - values_unwrapped(:, c)).^2;
             dMs = 1000*d/(2*pi);
         
             %dd = sqrt(sum(d)/(nF - 2)); 
-            latencyErrs(cp, c) = sqrt(sum(dMs)/(nF - 2));
+            latencyErrs(rc, c) = sqrt(sum(dMs)/(nF - 2));
         end
     end
 end
---
+++ frequency/rcaExtra_computeSubjectLatencies.m
@@ -0,0 +1,45 @@
+function [latencyVals, latencyErrs] = rcaExtra_computeSubjectLatencies(rcaResult)
+% Function will compute latencies based on frequency info, 
+% stimulation frequency F and harmonic multiples, and angle values.
+
+% Input args: rcaResult structure
+% Fields used for computation:
+
+% rcaResult.rcaSettings.useFrequencies  -- cell array of harmonic multiples
+% rcaResult.rcaSettings.useFrequenciesHz -- double value of stimulus frequency
+% rcaResult.subjAvg.phase -- angle values, 4D matrix of nF x nRc x nSubj nCnd
+
+
+% Output args:
+
+% latencyVals 3D array, computed latencies (msec), nSubj x nRCs x nCnd
+% latencyErrs 3D array, computed latency error estimates (msec), symmetric,  nSubj x nRCs x nCnd
+
+    %
+    freqIdx = cellfun(@(x) str2double(x(1)), rcaResult.rcaSettings.useFrequencies, 'uni', true);
+    freqVals = rcaResult.rcaSettings.useFrequenciesHz*freqIdx';
+    
+    nF = numel(freqIdx);
+    [~, nComp, nSubj, nCnd] = size(rcaResult.subjAvg.phase);
+    latencyVals = zeros(nSubj, rcaResult.rcaSettings.nComp, nCnd);
+    latencyErrs = zeros(nSubj, rcaResult.rcaSettings.nComp, nCnd);
+    for s = 1:nSubj
+        for rc = 1:nComp
+            for c = 1:nCnd
+                rcaAngles = squeeze(rcaResult.subjAvg.phase(freqIdx, rc, s, c));
+                values_unwrapped = unwrapPhases(rcaAngles);
+
+                [Pc, ~] = polyfit(freqVals, values_unwrapped, 1);
+       
+                yfit = Pc(1)*freqVals + Pc(2);
+    
+                latencyVals(s, rc, c) = 1000*Pc(1)/(2*pi);    
+                d = (yfit - values_unwrapped).^2;
+                dMs = 1000*d/(2*pi);
+        
+                %dd = sqrt(sum(d)/(nF - 2)); 
+                latencyErrs(s, rc, c) = sqrt(sum(dMs)/(nF - 2));
+            end
+        end
+    end
+end

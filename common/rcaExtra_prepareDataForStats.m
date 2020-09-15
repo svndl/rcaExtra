d311d8d4217e3c70f7c31325247607ab96c93416
leksea <leksea@gmail.com>
2020-09-15 09:47:32 -0700

    Updated/limited functionality
    Removed sourceData and disabled sensor-space stats functionality

--- common/rcaExtra_prepareDataForStats.m
+++ common/rcaExtra_prepareDataForStats.m
@@ -1,12 +1,16 @@
-function [subjRCMean, subjSensorMean] = rcaExtra_prepareDataForStats(dataStruct, dataSettings)
+function subjRCMean = rcaExtra_prepareDataForStats(rcaResult)
 
 % Averages and concatenates subject's RCs and Sensor data for stat analysis
 % Alexandra Yakovleva, Stanford Unversity 2020
 
-    subjRCMean = rcaExtra_prepareDataArrayForStats(dataStruct.projectedData, dataSettings);
-    % repeat for mean sensor data
-    dataSettings_source = dataSettings;
-    
-    dataSettings_source.nComp = 128;
-    subjSensorMean = rcaExtra_prepareDataArrayForStats(dataStruct.sourceData, dataSettings_source);
+    % copy RC Settings 
+    dataSettings = rcaResult.rcaSettings;
+    subjRCMean = rcaExtra_prepareDataArrayForStats(rcaResult.projectedData, dataSettings);
+
+%% N/A for source data at the moment  
+%     % copy rcaSettings and replace # of Rcs with # of electrodes for mean sensor data
+%     dataSettings_source = dataSettings;
+%     
+%     dataSettings_source.nComp = 128;
+%     subjSensorMean = rcaExtra_prepareDataArrayForStats(rcaResult.sourceData, dataSettings_source);
 end

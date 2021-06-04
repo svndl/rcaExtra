f6907c50b87fcfd77b1c35277bd6cbc1ecdfd53a
leksea <leksea@gmail.com>
2021-06-04 14:04:46 -0700

    Support single frequency filtering

--- toolbox/rcaExtra_filterData.m
+++ toolbox/rcaExtra_filterData.m
@@ -21,6 +21,8 @@ function dataOut = rcaExtra_filterData(rawData, freqsPresentHz, harmonicSelectio
     if (~iscell(rawData))
         rawData = {rawData};
     end
+    % pre-allocate/define the return values
+    
     cleanSignal = cell(size(rawData));
     nFSignal = cell(size(rawData));
     
@@ -30,14 +32,22 @@ function dataOut = rcaExtra_filterData(rawData, freqsPresentHz, harmonicSelectio
     % shortest epoch to contain integer cycles of all frequencies
     nEpochsMax = min(trialSizeSamples./cycleSizeSamplesPresent);
     minEpochDurationSamples = trialSizeSamples/nEpochsMax;
- 
-    freqsSplitHz_nF1 = freqsPresentHz(1):freqsPresentHz(1):DAQ_Rate/2;
-    freqsSplitHz_nF2 = freqsPresentHz(2):freqsPresentHz(2):DAQ_Rate/2;
+    
+    % workaround for only one frequency present
+    if( numel(freqsPresentHz) == 1)
+        freqsPresentHz_2F = [freqsPresentHz, freqsPresentHz];
+    end
 
+    % compute nF1, nF2 frequencies
+    freqsSplitHz_nF1 = freqsPresentHz_2F(1):freqsPresentHz_2F(1):DAQ_Rate/2;
+    freqsSplitHz_nF2 = freqsPresentHz_2F(2):freqsPresentHz_2F(2):DAQ_Rate/2;
+
+    %  compute nF1, nF2 periods
+    
     filteredFreqPeriods_nF1 = floor(freqsSplitHz_nF1*minEpochDurationSamples./DAQ_Rate);
     filteredFreqPeriods_nF2 = floor(freqsSplitHz_nF2*minEpochDurationSamples./DAQ_Rate);
     
-    % compute frequency multiples to be used for 
+    % compute frequency multiples to be used/removed  
     switch harmonicSelection
         case 'nF1'
             filteredFreqPeriods = filteredFreqPeriods_nF1;
@@ -58,11 +68,15 @@ function dataOut = rcaExtra_filterData(rawData, freqsPresentHz, harmonicSelectio
     % up to Nyquist Frequency
     filteredFreqPeriodsNq = filteredFreqPeriods(filteredFreqPeriods < round(minEpochDurationSamples/2));
 
+    % for every element in rawData, reshape trial duration nTrialSamples to minEpochDurationSamples x nEpochs 
     resampledDataCell =  cellfun(@(x) reshapeTrialToEpochs(x, minEpochDurationSamples), rawData, ...
         'uni', false);
+    
+    % filter data
     [cleanSignal, nFSignal] = cellfun(@(x) rcaExtra_filter4DData(x, filteredFreqPeriodsNq), ...
         resampledDataCell, 'uni', false);
     
+    % return clean or filtered out data  
     switch filterOperation
         case 'keep'
             dataOut = nFSignal;

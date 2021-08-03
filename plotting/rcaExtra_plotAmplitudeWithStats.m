dcdd31879b6c51f947fc3753842c16ab4ac27afd
leksea <leksea@gmail.com>
2021-08-02 18:25:49 -0700

    updated code

--- plotting/rcaExtra_plotAmplitudeWithStats.m
+++ plotting/rcaExtra_plotAmplitudeWithStats.m
@@ -3,12 +3,15 @@ function rcaExtra_plotAmplitudeWithStats(plotContainer1, plotContainer2, rcResul
     statsBetween_rcaData = rcaExtra_runStatsAnalysis(rcResultContainer1, rcResultContainer2);
     figureHandles = rcaExtra_plotAmplitudes(plotContainer1, plotContainer2);
     xLocations = 1:size(statsBetween_rcaData.pValues, 1);
-    for fh = 1:int8(numel(figureHandles))
-        statsBetween_rcaData.pValues(:, 1, fh)
-        figureHandles(fh).Name = char(strcat(plotContainer1.conditionLabels(fh), ...
-            'SA',plotContainer1.dataLabel, plotContainer2.dataLabel));
-        currAxisHandle = get(figureHandles(fh), 'CurrentAxes');
-        plot_addStatsBar_freq(currAxisHandle, statsBetween_rcaData.pValues(:, 1, fh), ...
-            xLocations');
+    [nRcs, nCnds] = size(figureHandles);
+    
+    for rc = 1:nRcs
+        for cn = 1:nCnds
+            figureHandles(rc, cn).Name = char(strcat(plotContainer1.conditionLabels(cn), ...
+                'SA',plotContainer1.dataLabel, plotContainer2.dataLabel));
+            currAxisHandle = get(figureHandles(rc, cn), 'CurrentAxes');
+            plot_addStatsBar_freq(currAxisHandle, statsBetween_rcaData.pValues(:, rc, cn), ...
+                xLocations');
+        end
     end
 end

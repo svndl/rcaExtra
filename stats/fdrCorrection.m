5971ee44b1169bbe782adb7b797b25371c512498
Alexandra Yakovleva <leksea@gmail.com>
2022-04-05 17:40:53 -0700

    Adding fdr correction
    Initial commit

---
+++ stats/fdrCorrection.m
@@ -0,0 +1,28 @@
+function h = fdrCorrection(pvals, q)
+% pvals: p-values of your multiple significance tests
+% q: desired FDR level (like alpha) -- defaults to 0.05
+% h: array of size(pvals) indicating the corrected hypothesis test results:
+% 1 indicates a rejected null hypothesis
+% reference: Benjamini, Yoav; Yekutieli, Daniel (2001). "The control of the false discovery rate in multiple testing under dependency". Annals of Statistics 29 (4): 11651188.
+    sz = size(pvals); 
+    pvals = pvals(:);  
+    m = length(pvals);
+    if size(nargin) < 2 
+        q=0.05; 
+    end
+    % sort pValues
+    [c, d] = sort(pvals, 'ascend');
+    
+    mxind = max(find( (c <= (1:m)'/m*q) ), 'first');
+    h = zeros(size(pvals));
+    h(d(1:mxind)) = 1;  
+    h = reshape(h,sz(1),sz(2));
+    
+%% write to excel
+
+    T1 = table(pvals, h, ...
+        'VariableNames', {'p-Values','MD'});
+    writetable(T1, fullfile(pathCSV, 'fdr.csv'),'Delimiter',',');
+
+
+

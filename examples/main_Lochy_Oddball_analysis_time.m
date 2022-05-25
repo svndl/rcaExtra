becb6f33fdfac9eebfd92480c12de16722024368
leksea <leksea@gmail.com>
2022-05-25 09:56:33 -0700

    Updated example

--- examples/main_Lochy_Oddball_analysis_time.m
+++ examples/main_Lochy_Oddball_analysis_time.m
@@ -68,12 +68,12 @@ function main_Lochy_Oddball_analysis_time
     rcResult_all = rcaExtra_runAnalysis(runSettings_2hz, EEGData);
     
     % filter out 6Hz
-    data_nF1 = rcaExtra_filterData(EEGData, analysisStruct.info.frequenciesHz, 'nF1clean', 'keep');
+    data_nF1 = rcaExtra_filterData(EEGData, analysisStruct.info.frequenciesHz, 'nF1clean', 'keep');    
     
-    runSettings_1hz_clean = runSettings_2hz;
-    runSettings_1hz_clean.label = 'Conditions_1-4_nF1_clean';
+    runSettings_2hz_clean = runSettings_2hz;
+    runSettings_2hz_clean.label = 'Conditions_1-4_nF1_clean';
 
-    rcResult_all_nF1_clean = rcaExtra_runAnalysis(runSettings_1hz_clean, data_nF1);
+    rcResult_all_nF1_clean = rcaExtra_runAnalysis(runSettings_2hz_clean, data_nF1);
     
     % flip weights, use once
     %rcResult_all_nF1_clean = rcaExtra_adjustRCWeights(rcResult_all_nF1_clean, [-1 2 3 4 5 6]);
@@ -91,13 +91,35 @@ function main_Lochy_Oddball_analysis_time
     plot_1hz_all = rcaExtra_initPlottingContainer(rcResult_all_nF1_clean);
     plot_1hz_all.conditionLabels = analysisStruct.info.conditionLabels;
     plot_1hz_all.rcsToPlot = 1;
-    plot_1hz_all.cndsToPlot = [1:numel(analysisStruct.info.conditionLabels)];
+    plot_1hz_all.cndsToPlot = 1:4;
     plot_1hz_all.conditionColors = colors_to_use./255;
     % plots groups, each condition in separate window
     rcaExtra_plotWaveforms(plot_1hz_all)
     
     %split data to be displayed in one figure
-    [c1, c2, c3, c4, c5]  = rcaExtra_splitPlotDataByCondition(plot_1hz_all);
-    rcaExtra_plotWaveforms(c1, c2, c3, c4, c5)
+    [c1_rc, c2_rc, c3_rc, c4_rc]  = rcaExtra_splitPlotDataByCondition(plot_1hz_all);
+    rcaExtra_plotWaveforms(c1_rc, c2_rc, c3_rc, c4_rc);
+    
+
+%     %% merging two conditions
+%     data_nF1_clean_c12 = rcaExtra_mergeDatasetConditions(data_nF1, [1, 2]);
+%     rcResult_c12_nF1_clean = rcResult_all_nF1_clean;
+%     rcResult_c12_nF1_clean.label = 'Conditions_1-2_nF1_clean';
+%     rcResult_c12_nF1_clean.useCnds = [1 2];
+%     rcResult_c12_nF1_clean = rcaExtra_runAnalysis(runSettings_2hz_clean, data_nF1_clean_c12);
+%     
+%     %% projecting conditions 3, 4 through weights
+%     projected_c34_nF1_clean = projectDatasets(rcResult_c12_nF1_clean.W, data_nF1(:, 3), data_nF1(:, 4));
+    
+    %% Averaging at Sensor Data
+    avgSensor_all = averageSensor_time(rcResult_all_nF1_clean.rcaSettings, data_nF1);
+    plot_ch_65 = rcaExtra_initPlottingContainer(avgSensor_all);
+    plot_ch_65.conditionLabels = plot_1hz_all.conditionLabels;
+    plot_ch_65.rcsToPlot = 65;
+    plot_ch_65.cndsToPlot = [1 2 3 4];
+    plot_ch_65.conditionColors = colors_to_use./255;
+    [c1_s, c2_s, c3_s, c4_s] = rcaExtra_splitPlotDataByCondition(plot_ch_65);
+    rcaExtra_plotWaveforms(c1_s, c2_s, c3_s, c4_s);
+    
         
 end

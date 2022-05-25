de3fb740669dee020a2ca84713e4654fd0853ef7
leksea <leksea@gmail.com>
2022-05-25 16:38:08 -0700

    Updates for example dataset (Lochy)

--- examples/loadExperimentInfo_Lochy_Oddball.m
+++ examples/loadExperimentInfo_Lochy_Oddball.m
@@ -1,39 +1,39 @@
-function adultsLochyzInfo = loadExperimentInfo_Lochy_Oddball
+function adultsLochyInfo = loadExperimentInfo_Lochy_Oddball
     
     %% info is a structure describing experiment parameters
-    adultsLochyzInfo = rcaExtra_genStructureTemplate;
+    adultsLochyInfo = rcaExtra_genStructureTemplate;
     [curr_path, ~, ~] = fileparts(mfilename('fullpath'));
-    adultsLochyzInfo.path.rootFolder = curr_path;
+    adultsLochyInfo.path.rootFolder = curr_path;
     
     % source EEG data
     srcDirPath = uigetdir(curr_path, 'Select EEG SOURCE directory');
     if (~isempty(srcDirPath))
-        adultsLochyzInfo.path.sourceEEGDir = srcDirPath;
+        adultsLochyInfo.path.sourceEEGDir = srcDirPath;
     end
         
     % destination EEG directory
-    adultsLochyzInfo.path.destDataDir = uigetdir(curr_path, 'Select analysis results directory');
+    adultsLochyInfo.path.destDataDir = uigetdir(curr_path, 'Select analysis results directory');
     
 
     %% create subdirectories
     dirNames = {'MAT', 'FIG', 'RCA'};
-    dirPaths = rcaExtra_setupDestDir(adultsLochyzInfo.path.destDataDir, dirNames);
+    dirPaths = rcaExtra_setupDestDir(adultsLochyInfo.path.destDataDir, dirNames);
     
-    adultsLochyzInfo.path.destDataDir_MAT = dirPaths{1};
-    adultsLochyzInfo.path.destDataDir_FIG = dirPaths{2};
-    adultsLochyzInfo.path.destDataDir_RCA = dirPaths{3};
+    adultsLochyInfo.path.destDataDir_MAT = dirPaths{1};
+    adultsLochyInfo.path.destDataDir_FIG = dirPaths{2};
+    adultsLochyInfo.path.destDataDir_RCA = dirPaths{3};
    
     % replace default values here
-    adultsLochyzInfo.info.subjTag = 'nl*';
-    adultsLochyzInfo.info.subDirTxt = 'Exp_TEXT_HCN_128_Avg';
-    adultsLochyzInfo.info.subDirMat = 'Exp_MATL_HCN_128_Avg';
-    adultsLochyzInfo.info.groupLabels = {'Lochy Oddball'};
-    adultsLochyzInfo.info.conditionLabels = {'Word vs Psudofont', 'Word vs Nonword',...
+    adultsLochyInfo.info.subjTag = 'nl*';
+    adultsLochyInfo.info.subDirTxt = 'Exp_TEXT_HCN_128_Avg';
+    adultsLochyInfo.info.subDirMat = 'Exp_MATL_HCN_128_Avg';
+    adultsLochyInfo.info.groupLabels = {'Lochy Oddball'};
+    adultsLochyInfo.info.conditionLabels = {'Word vs Psudofont', 'Word vs Nonword',...
         'Word vs Pseudoword', 'Word vs Psudofont jitter,', ' Word vs Psudofont evenball'};
-    adultsLochyzInfo.info.frequenciesHz = [2, 10];
-    adultsLochyzInfo.info.useSpecialDataLoader = false;
-    adultsLochyzInfo.info.frequencyLabels = {{'1F1' '2F1' '3F1' '4F1'}, {'1F2' '2F2'}};
-    adultsLochyzInfo.info.binsNmb = 10;
+    adultsLochyInfo.info.frequenciesHz = [2, 10];
+    adultsLochyInfo.info.useSpecialDataLoader = false;
+    adultsLochyInfo.info.frequencyLabels = {{'1F1' '2F1' '3F1' '4F1'}, {'1F2' '2F2'}};
+    adultsLochyInfo.info.binsNmb = 10;
 end
 
 
--- examples/main_Lochy_Oddball_analysis_time.m
+++ examples/main_Lochy_Oddball_analysis_time.m
@@ -1,11 +1,22 @@
 function main_Lochy_Oddball_analysis_time
 
-    %% define experimentInfo
-          
+    %% Loading sensor-space data
+    
+    % You'll need to create 'loadExperimentInfo_ + experimentName.m file
+    % that is going to contain details about your source data:
+    
+    % first few letters or numbers of all filenames, like nl*, BLC*, etc
+    % if data exports are in subdirectory (nl*/time/RawTrials)
+    % what frequencies are present in the dataset (for reverse-engineeering epoch length) 
+    % see loadExperimentInfo_Lochy_Oddball.m
+    
+    
     experimentName = 'Lochy_Oddball';
     
+    
     % load up expriment info specified in loadExperimentInfo_experimentName
-    % matlab file
+    % matlab file. Lines 21-28 are generic and can be copy-pasted  
+    % analysisStruct contains info about data location and data properties
     
     try
         analysisStruct = feval(['loadExperimentInfo_' experimentName]);
@@ -16,110 +27,151 @@ function main_Lochy_Oddball_analysis_time
         analysisStruct = loadExperimentInfo_Default;
     end
     
-    % specified for general data loader and plotting, currently not used
+    
+    % analysisStruct.domain will be propagated to rcaSetting, stats, plotting and needs to
+    % be defined for the use of any high-level function 
     analysisStruct.domain = 'time';
+    
     loadSettings = rcaExtra_getDataLoadingSettings(analysisStruct);
     
     % read raw data 
     [subjList, EEGData] = getRawData(loadSettings);
+    
+    %% RCA 
+    % get generic template for RCA settings
     rcSettings = rcaExtra_getRCARunSettings(analysisStruct);
+    
+    %Filling out the template with subjects list:
     rcSettings.subjList = subjList;
-       
-    % selecting specific frequency (1 HZ) and corresponding sampling rate
-    % and time course length to use for resampling, RC analysis and
-    % plotting results
+        
+    %copy settings template to 2hz analysis template 
+    runSettings_c1234_2hz = rcSettings;
+    % use 2Hz cycle length and duration 
+    runSettings_c1234_2hz.cycleLength = rcSettings.cycleLength(1);
+    runSettings_c1234_2hz.cycleDuration = rcSettings.cycleDuration(1);
+    
+    % the name under which RCA result will be saved inyour output/RCA directory
+    
+    runSettings_c1234_2hz.label = 'Conditions_1-4';
+    runSettings_c1234_2hz.computeStats = 0;
+    runSettings_c1234_2hz.useCnds = 1:4;
 
-    % not going to work because 1F1 = 6F2
-%     data_nF2 = rcaExtra_filterData(EEGData, analysisStruct.info.frequenciesHz, 'nF2', 'keep');
+    % demonstrates that RCA result has 10Hz present in and not informative because it overpowers 2Hz response    
+    rcResult_c1234 = rcaExtra_runAnalysis(runSettings_c1234_2hz, EEGData);
     
-    % rcSettings has two resampling values and two timecourses
-    
-    runSettings_2hz = rcSettings;
-    runSettings_2hz.cycleLength = rcSettings.cycleLength(1);
-    runSettings_2hz.cycleDuration = rcSettings.cycleDuration(1);
-    
-    
-    nConditions = size(EEGData, 2);
-    % allocate cell array to store individual condition RCA runtime settings 
-    runSettings_1hz_condition = cell(nConditions, 1);
-    
-    % allocate cell array to store individual condition RCA runtime results     
-    rcResult_condition = cell(nConditions, 1);
-    
-    % run RC analysis for each condition
-%     for nc = 1:nConditions
-%         % copy runtime settings from 1Hz template
-%         runSettings_1hz_condition{nc} = runSettings_1hz;
-%         runSettings_1hz_condition{nc}.useCnds = nc;
-%         % add condition-specific label 
-%         runSettings_1hz_condition{nc}.label = analysisStruct.info.conditionLabels{nc};
-%         
-%         % select subset of raw data        
-%         rcResult_condition{nc} = rcaExtra_runAnalysis(runSettings_1hz_condition{nc}, EEGData);
-%     end
-    
-    
-    % run RC on all conditions and project
-    runSettings_2hz.label = 'Conditions_1-4';
-    
-    % demonstrate that RCA result has 6Hz present in and not informative
-    runSettings_2hz.computeStats = 0;
-    runSettings_2hz.useCnds = 1:4;
-    rcResult_all = rcaExtra_runAnalysis(runSettings_2hz, EEGData);
-    
-    % filter out 6Hz
+    %% Filtering out 10 Hz
     data_nF1 = rcaExtra_filterData(EEGData, analysisStruct.info.frequenciesHz, 'nF1clean', 'keep');    
     
-    runSettings_2hz_clean = runSettings_2hz;
-    runSettings_2hz_clean.label = 'Conditions_1-4_nF1_clean';
+    runSettings_c1234_2hz_nF1clean = runSettings_c1234_2hz;
+    runSettings_c1234_2hz_nF1clean.label = 'Conditions_1-4_nF1_clean';
+    rcResult_c1234_nF1clean = rcaExtra_runAnalysis(runSettings_c1234_2hz_nF1clean, data_nF1);
 
-    rcResult_all_nF1_clean = rcaExtra_runAnalysis(runSettings_2hz_clean, data_nF1);
+    %%  Weight Flipping (match with xDiva waveform polarity)
     
-    % flip weights, use once
+    % function will use command line prompt:
+    % do you want to save the results? 
+    % (Y) Do you want to save results as new matfile?
+    % Vector specifies both desired order and polarity: [-1 2 3 4 5 6]
+    % To change order [-2 1 3 4 5 6]
     %rcResult_all_nF1_clean = rcaExtra_adjustRCWeights(rcResult_all_nF1_clean, [-1 2 3 4 5 6]);
     
-    % plot conditions
     
-    % colors, make sure to have enough for each condition
+    %% Run RCA on individual conditions
+    
+%     nConditions = size(data_nF1, 2);
+%     % allocate cell array to store individual condition RCA runtime settings 
+%     runSettings_cnd_2hz_condition = cell(nConditions, 1);
+%     
+%     % allocate cell array to store individual condition RCA runtime results     
+%     rcResult_condition = cell(nConditions, 1);
+%     
+%     % use 2Hz conditions 1-4 
+%     for nc = 1:nConditions - 1
+%         
+%     % copy runtime settings from 2Hz nF1clean template
+%         runSettings_cnd_2hz_condition{nc} = runSettings_c1234_2hz_nF1clean;
+%         runSettings_cnd_2hz_condition{nc}.useCnds = nc;
+%         
+%         add condition-specific label
+%         runSettings_cnd_2hz_condition{nc}.label = analysisStruct.info.conditionLabels{nc};
+% 
+%         % select subset of raw data
+%         rcResult_condition{nc} = rcaExtra_runAnalysis(runSettings_cnd_2hz_condition{nc}, data_nF1);
+%     end
+%     
+
+    %% Run RCA on merged conditions 
+    
+    % merging two conditions
+    data_nF1_clean_c12 = rcaExtra_mergeDatasetConditions(data_nF1, [1, 2]);
+    runSettings_2hz_c12_nF1clean = runSettings_c1234_2hz_nF1clean;
+    runSettings_2hz_c12_nF1clean.label = 'Conditions_1-2_nF1_clean';
+    runSettings_2hz_c12_nF1clean.useCnds = 1;
+    rcResult_2hz_c12_nF1clean = rcaExtra_runAnalysis(runSettings_2hz_c12_nF1clean, data_nF1_clean_c12);
+     
+    %% Project through learned weights
+    % projecting conditions 3, 4 through weights learned on combined [1, 2]
+    
+    [projected_c3_nF1_clean, projected_c4_nF1_clean] = ...
+        rcaExtra_projectDataSubset(rcResult_2hz_c12_nF1clean, data_nF1(:, 3), data_nF1(:, 4));
+    
+    
+    %% Plot
+    % package with colors, make sure to have enough for each condition
     load('colorbrewer');
-    
+    % Rows = colors, columns = RGB values in 1-255 range (need to be normalized by /255) 
     colors_to_use = colorbrewer.qual.Set1{8};
         
-    %% plotting
-    rcResult_all_nF1_clean.rcaSettings.computeStats = 0;
+    %% plotting all rcResult_all_nF1_clean
+    % do not use stats for now
+    rcResult_c1234_nF1clean.rcaSettings.computeStats = 0;
 
-    plot_1hz_all = rcaExtra_initPlottingContainer(rcResult_all_nF1_clean);
-    plot_1hz_all.conditionLabels = analysisStruct.info.conditionLabels;
-    plot_1hz_all.rcsToPlot = 1;
-    plot_1hz_all.cndsToPlot = 1:4;
-    plot_1hz_all.conditionColors = colors_to_use./255;
-    % plots groups, each condition in separate window
-    rcaExtra_plotWaveforms(plot_1hz_all)
+    plot_2hz_nF1clean_c14 = rcaExtra_initPlottingContainer(rcResult_c1234_nF1clean);
+    plot_2hz_nF1clean_c14.conditionLabels = analysisStruct.info.conditionLabels;
+    plot_2hz_nF1clean_c14.rcsToPlot = 1;
+    plot_2hz_nF1clean_c14.cndsToPlot = 1:4;
+    plot_2hz_nF1clean_c14.conditionColors = colors_to_use./255;
     
-    %split data to be displayed in one figure
-    [c1_rc, c2_rc, c3_rc, c4_rc]  = rcaExtra_splitPlotDataByCondition(plot_1hz_all);
+    % plots groups, each condition in separate window
+    rcaExtra_plotWaveforms(plot_2hz_nF1clean_c14)
+    
+    % split data to be displayed in one figure
+    [c1_rc, c2_rc, c3_rc, c4_rc]  = rcaExtra_splitPlotDataByCondition(plot_2hz_nF1clean_c14);
     rcaExtra_plotWaveforms(c1_rc, c2_rc, c3_rc, c4_rc);
     
-
-%     %% merging two conditions
-%     data_nF1_clean_c12 = rcaExtra_mergeDatasetConditions(data_nF1, [1, 2]);
-%     rcResult_c12_nF1_clean = rcResult_all_nF1_clean;
-%     rcResult_c12_nF1_clean.label = 'Conditions_1-2_nF1_clean';
-%     rcResult_c12_nF1_clean.useCnds = [1 2];
-%     rcResult_c12_nF1_clean = rcaExtra_runAnalysis(runSettings_2hz_clean, data_nF1_clean_c12);
-%     
-%     %% projecting conditions 3, 4 through weights
-%     projected_c34_nF1_clean = projectDatasets(rcResult_c12_nF1_clean.W, data_nF1(:, 3), data_nF1(:, 4));
+    %% Stats
+    % let's add stats computing 
+    statData = rcaExtra_runStatsAnalysis(projected_c3_nF1_clean, projected_c4_nF1_clean);    
     
-    %% Averaging at Sensor Data
-    avgSensor_all = averageSensor_time(rcResult_all_nF1_clean.rcaSettings, data_nF1);
+    % specify plotting
+    plot_2hz_nF1clean_c3 = rcaExtra_initPlottingContainer(projected_c3_nF1_clean);
+    plot_2hz_nF1clean_c3.conditionLabels = analysisStruct.info.conditionLabels(1);
+    plot_2hz_nF1clean_c3.rcsToPlot = 1;
+    plot_2hz_nF1clean_c3.cndsToPlot = 1;
+    plot_2hz_nF1clean_c3.conditionColors = colors_to_use(1, :)./255;
+    
+    plot_2hz_nF1clean_c4 = rcaExtra_initPlottingContainer(projected_c4_nF1_clean);
+    plot_2hz_nF1clean_c4.conditionLabels = analysisStruct.info.conditionLabels(2);
+    plot_2hz_nF1clean_c4.rcsToPlot = 1;
+    plot_2hz_nF1clean_c4.cndsToPlot = 1;
+    plot_2hz_nF1clean_c4.conditionColors = colors_to_use(2, :)./255;
+ 
+    % plot without stats
+    fig_c34 = rcaExtra_plotWaveforms(plot_2hz_nF1clean_c3,plot_2hz_nF1clean_c4);
+    % add stats data
+    
+    axes_h = get(fig_c34,'CurrentAxes');
+    plot_addStatsBar_time(axes_h, statData.pValues, statData.sig, projected_c3_nF1_clean.timecourse)
+    
+    %% Sensor Data
+    avgSensor_all = averageSensor_time(rcResult_c1234_nF1clean.rcaSettings, data_nF1);
     plot_ch_65 = rcaExtra_initPlottingContainer(avgSensor_all);
-    plot_ch_65.conditionLabels = plot_1hz_all.conditionLabels;
+    plot_ch_65.conditionLabels = plot_2hz_nF1clean_c14.conditionLabels;
     plot_ch_65.rcsToPlot = 65;
     plot_ch_65.cndsToPlot = [1 2 3 4];
     plot_ch_65.conditionColors = colors_to_use./255;
     [c1_s, c2_s, c3_s, c4_s] = rcaExtra_splitPlotDataByCondition(plot_ch_65);
     rcaExtra_plotWaveforms(c1_s, c2_s, c3_s, c4_s);
-    
+    rcaExtra_plotWaveforms(c1_s, c1_rc);
         
 end
